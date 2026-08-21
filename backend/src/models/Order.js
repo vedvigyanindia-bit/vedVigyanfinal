f6a05d06bb640f76b99ff9b69b4e4ee5ea51ab4e
mahimamj <joshimahima798@gmail.com>
2026-08-21 17:22:35 +0530

    Disable Mongoose query buffering to prevent 10s timeout when IP is restricted

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -167,7 +167,7 @@ class OrderRepository {
       const uri = process.env.MONGODB_URI || "mongodb+srv://vedvigyanindia_db_user:5wZiQXVTatLkjEHe@cluster0.ry3emxv.mongodb.net/ved_vigyan?retryWrites=true&w=majority";
       if (mongoose && uri && mongoose.connection.readyState !== 1) {
         await mongoose.connect(uri, {
-          bufferCommands: true,
+          bufferCommands: false,
           serverSelectionTimeoutMS: 5000
         });
         console.log('[Order DB] Connected to MongoDB Atlas on demand.');
